525355bf81c82dda3ee1a4c4635bcdaa4ac4fd31
Imadlmair <116261917+Imadlmair@users.noreply.github.com>
2022-10-20 12:21:11 +0200

    Add files via upload
    This is the latest version.

--- Program.cs
+++ Program.cs
@@ -9,42 +9,50 @@ string answer = Console.ReadLine()!;
 
 
 
-if (answer == "yes" ||answer == "YES")
+if (answer == "yes" || answer == "YES")
 {
     Console.WriteLine("How many point did you get? ");
     points += int.Parse(Console.ReadLine()!);
 }
-else {possiblepoints-=20;}
+
+else { possiblepoints -= 20; }
+
 Console.Write("Did you write the second exam? ");
 answer = Console.ReadLine()!;
-
-if (answer == "yes" ||answer == "YES")
+if (answer == "yes" || answer == "YES")
 {
     Console.WriteLine("How many point did you get? ");
     points += int.Parse(Console.ReadLine()!);
 }
-else {possiblepoints-=20;}
+
+else { possiblepoints -= 20; }
 
 Console.WriteLine("How many points did you get in your Homework?");
 points += int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("How many points did you get in your Oral Exam?");
-points += int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Did you do the Oral Exam? ");
+answer = Console.ReadLine()!;
+if (answer == "yes" || answer == "YES")
+{
+    Console.WriteLine("How many Points did yo get? ");
+    points += int.Parse(Console.ReadLine()!);
+}
+else { possiblepoints -= 20; }
 
-double percent = 100d*points/possiblepoints;
-if (percent>=89)
+double percent = 100d * points / possiblepoints;
+if (percent >= 89)
 {
     grade = 1;
 }
-else if (percent>=76)
+else if (percent >= 76)
 {
     grade = 2;
 }
-else if (percent>=63)
+else if (percent >= 63)
 {
     grade = 3;
 }
-else if (percent>=50)
+else if (percent >= 50)
 {
     grade = 4;
 }
